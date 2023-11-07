858655c7423ac33f68f4f2b7051d4871ccf0ed29
Adrian Izquierdo <adrian.izquierdo@estudiants.urv.cat>
2023-11-07 23:32:26 +0100

    Ya se ha cambiado el programa

--- main.cpp
+++ main.cpp
@@ -10,20 +10,21 @@ DigitalOut led(D4);
 // main() runs in its own thread in the OS
 int main()
 {
-    led=1;
+    led=0;
     float tempDifference, currentTemp, desiredTemp, tempIncrement=0;
     
     while (true) {
-        currentTemp= tempSensor.readTemperature()+tempDifference;
+        currentTemp= tempSensor.readTemperature();
         desiredTemp= rotatoryAngleSensor.read_u16()/1000;
 
-        //targetMin + (value - sourceMin) * (targetMax - targetMin) / (sourceMax - sourceMin);
-        desiredTemp= 5 + (desiredTemp-0) * (35 - 5) / (65 - 0); //Limit the desired temp from 5 to 35 celcius
-        printf("currentTemp: %.2fºC\ndesiredTemp:%.2fºC\n", currentTemp, desiredTemp);
-        tempDifference=desiredTemp-currentTemp;
-        if (tempDifference!=currentTemp){
-            printf("Changing temperature to match desired temperature.\n");
+        if(currentTemp < desiredTemp)
+        {
+            led = !led;
+        }else {
+            led = 1;
         }
+        //Funcion para escribir en el LCD
+
         ThisThread::sleep_for(1500ms);
         
     }
